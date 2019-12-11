d6c4903edcdf69136a06fd1c376129103716ac8a
king <519421214@qq.com>
2019-12-11 11:22:06 +0800

    ocr小程序接口

--- src/main/java/com/king/hhczy/controller/BaiduController.java
+++ src/main/java/com/king/hhczy/controller/BaiduController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
@@ -31,6 +33,20 @@ public class BaiduController {
      */
     @PostMapping(value = "/ocr")
     public ResponseEntity<Map> ocr(MultipartFile file) throws Exception{
+        //接收图像二进制数据
+        byte[] buf = file.getBytes();
+        return ResponseEntity.ok(baiduService.ocr(buf));
+    }
+    /**
+     * 图片文字识别、中英文识别
+     * @return
+     * @throws Exception
+     */
+    @PostMapping(value = "/ocr-image")
+    public ResponseEntity<Map> ocrImage(HttpServletRequest request) throws Exception{
+        //获取从前台传过来得图片
+        MultipartHttpServletRequest req =(MultipartHttpServletRequest)request;
+        MultipartFile file = req.getFile("file");
         //接收图像二进制数据
         byte[] buf = file.getBytes();
         return ResponseEntity.ok(baiduService.ocr(buf));
