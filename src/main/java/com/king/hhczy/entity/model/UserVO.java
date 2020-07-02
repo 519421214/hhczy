fdba953e47a922b3e464d1bfeaf277155cb06d40
ningjinxiang <519421214@qq.com>
2020-07-02 09:25:47 +0800

    阿里的读取excel

--- src/main/java/com/king/hhczy/entity/model/UserVO.java
+++ src/main/java/com/king/hhczy/entity/model/UserVO.java
@@ -1,6 +1,7 @@
 package com.king.hhczy.entity.model;
 
 import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
@@ -10,6 +11,10 @@ import lombok.Data;
 @JsonNaming(PropertyNamingStrategy.UpperCamelCaseStrategy.class)
 public class UserVO {
     //回参参(反序列化)属性限定首字母大写
+    //用于转Json
     @JSONField(name = "Username")
     private String username;
+    //用于接参
+    @JsonProperty("Password")
+    private String password;
 }
---
+++ src/test/java/bean/DemoData.java
@@ -0,0 +1,10 @@
+package bean;
+
+import lombok.Data;
+
+@Data
+public class DemoData {
+    private String string1;
+    private String string2;
+    private String string3;
+}
