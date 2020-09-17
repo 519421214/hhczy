42ec0a723be269ab75a56fff47e55d5955244e6b
king <519421214@qq.com>
2020-09-17 08:43:36 +0800

    股市播报优化

--- src/main/java/com/king/hhczy/common/util/GupiaoUtil.java
+++ src/main/java/com/king/hhczy/common/util/GupiaoUtil.java
@@ -6,8 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.text.DecimalFormat;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -58,22 +57,55 @@ public class GupiaoUtil {
 
         String codesStr = collect.stream().reduce((s1, s2) -> s1 + "," + s2).orElse(null);
         DecimalFormat df = new DecimalFormat("0.00");
-
-        String forObject = restTemplate.getForObject(impUrl + "sh000001," + codesStr, String.class);
-        Arrays.stream(forObject.split(";")).map(x -> x.substring(x.indexOf("\"") + 1).split(",")).forEach(y -> {
-            if (y.length < 5) return;
+        //缓存
+        Map<String, Double> cacheHis = new HashMap<>();
+        //大盘当前
+        int aCur = 0;
+        //开启定时任务,执行定时播报
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                String forObject = restTemplate.getForObject(impUrl + "sh000001," + codesStr, String.class);
+                Arrays.stream(forObject.split(";")).map(x -> x.substring(x.indexOf("\"") + 1).split(",")).forEach(y -> {
+                    if (y.length < 5) return;
 //            System.out.println(y[0]+"\t[开]"+y[1]+"\t[低]"+y[5]+"\t[高]"+y[4]+"\t[当前]"+y[3]+"\t["+(Double.valueOf(y[3])>Double.valueOf(y[2])?"红":"绿")+"]"+
 //                    df.format(((Double.valueOf(y[3])/Double.valueOf(y[2]))-1)*100)+"%");
-            //获取盈亏占比
-            double ratio = (Double.valueOf(y[3]) / Double.valueOf(y[2])) - 1;
-            if(ratio>0){
-                WordsReading.speak("大盘红啦");
-            }
+                    //获取盈亏占比
+                    double ratio = (Double.valueOf(y[3]) / Double.valueOf(y[2])) - 1;
+                    //缓存点数
+                    cacheHis.put(y[0], ratio);
+                    StringBuffer sayWords = new StringBuffer();
+                    if ("上证指数".equals(y[0])) {
+                        if (aCur==0) {
+                            aCur = y[3];
+                        }
+                        Double szzs = cacheHis.get("上证指数");
+                        if (ratio-szzs>0.001) {
+                            sayWords.append("拉升了，拉升了，大A拉升了");
+                        }else if (ratio-szzs<0.001) {
+                            sayWords.append("跳水啦，大A崩盘啦，快跑");
+                        }
+                        if (ratio > 0) {
+                            if(szzs <0) sayWords.append("牛逼，大A翻红了");
+                        }else if (ratio<0){
+                            if(szzs >0) sayWords.append("卧槽，大A绿了");
+                        }
+                    }
+                    if (sayWords.length()>0) {
+                        WordsReading.speak(sayWords.toString());
+                    }
 //            String logPrint = String.format("%s\t" + "[开]%s\t" + "[低]%s\t" + "[高]%s\t" + "[NOW]%s\t" + "[%s]%s",
 //                    y[0], y[1], y[5], y[4], y[3],
 //                    ratio > 0 ? "红" : "绿",
 //                    df.format(ratio * 100) + "%");
 //            System.out.println(logPrint);
-        });
+                });
+            }
+        }, 0, 10000);
+    }
+
+    private void speakTimer(String[] codes) {
+
     }
 }
