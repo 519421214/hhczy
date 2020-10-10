b2725a4a7327ffd05d1059df6b1478f130ed9934
ningjinxiang <519421214@qq.com>
2020-10-10 16:07:44 +0800

    双色球数据同步

---
+++ src/main/java/com/king/hhczy/controller/BichromaticSphereController.java
@@ -0,0 +1,21 @@
+package com.king.hhczy.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 双色球开奖结果 前端控制器
+ * </p>
+ *
+ * @author ningjinxiang
+ * @since 2020-10-09
+ */
+@RestController
+@RequestMapping("/bichromatic-sphere")
+public class BichromaticSphereController {
+
+}
+
