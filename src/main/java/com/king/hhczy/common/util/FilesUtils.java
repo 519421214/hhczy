ba9b08dac8c57bca945896cfadb85632769f401d
king <519421214@qq.com>
2020-10-16 15:09:43 +0800

    适配优化

--- src/main/java/com/king/hhczy/common/util/FilesUtils.java
+++ src/main/java/com/king/hhczy/common/util/FilesUtils.java
@@ -1,5 +1,6 @@
 package com.king.hhczy.common.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.time.Instant;
 import java.util.*;
@@ -17,21 +19,24 @@ import java.util.*;
  * @author ningjinxiang
  * date 20190918
  */
+@Slf4j
 public class FilesUtils {
-	/**
-	 * 遍历获取整个文件夹（包括子文件夹）的文件路径(不遍历文件夹)
-	 * @param path
-	 * @return
-	 */
+    /**
+     * 遍历获取整个文件夹（包括子文件夹）的文件路径(不遍历文件夹)
+     * @param path
+     * @return
+     */
     public static List<Path> getAllFilesPaths(String path) {
-        Path startingDir = Paths.get(path);
+        return getAllFilesPaths(Paths.get(path));
+    }
+    public static List<Path> getAllFilesPaths(Path path) {
         List<Path> result = new LinkedList<Path>();
-		try {
-			Files.walkFileTree(startingDir, new FindJavaVisitor(result));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return result;
+        try {
+            Files.walkFileTree(path, new FindJavaVisitor(result));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
     public static boolean uploadFile(String parentPathStr,String fileName,byte[] buf) {
         fileName = Optional.ofNullable(fileName).orElse(Instant.now().toEpochMilli()+"");
@@ -44,20 +49,108 @@ public class FilesUtils {
         }
         return false;
     }
-
-    private static class FindJavaVisitor extends SimpleFileVisitor<Path> {
-        private List<Path> result;
-
-        public FindJavaVisitor(List<Path> result) {
-            this.result = result;
-        }
-
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-            result.add(file.getFileName());
-            return FileVisitResult.CONTINUE;
-        }
+    /**
+     * 获取文件夹的所有子文件个数
+     * @param path
+     * @return
+     */
+    public static Integer getDirFilesSize(String path) {
+        return getDirFilesSize(Paths.get(path));
     }
+    /**
+     * 获取文件创建、最后修改、最后访问时间戳Map
+     * @param path
+     * @return
+     */
+    public static Map<String, Long> getFileTime(String path) {
+        BasicFileAttributeView fileAttributeView = Files.getFileAttributeView(Paths.get(path), BasicFileAttributeView.class,
+                LinkOption.NOFOLLOW_LINKS);
+        Map<String, Long> result = new HashMap<>();
+        try {
+            BasicFileAttributes basicFileAttributes = fileAttributeView.readAttributes();
+            result.put("creationTime",basicFileAttributes.creationTime().toMillis());
+            result.put("lastModifiedTime",basicFileAttributes.lastModifiedTime().toMillis());
+            result.put("lastAccessTime",basicFileAttributes.lastAccessTime().toMillis());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    /**
+     * 指定获取文件创建、最后修改、最后访问时间戳
+     * @param path
+     * @return
+     */
+    public static Long getFileTime(String path,FilesAttribute filesAttribute) {
+        return getFileTime(Paths.get(path),filesAttribute);
+    }
+    public static Long getFileTime(Path path,FilesAttribute filesAttribute) {
+        BasicFileAttributeView fileAttributeView = Files.getFileAttributeView(path, BasicFileAttributeView.class,
+                LinkOption.NOFOLLOW_LINKS);
+        try {
+            BasicFileAttributes basicFileAttributes = fileAttributeView.readAttributes();
+            if (filesAttribute== FilesAttribute.CREATION_TIME) {
+                return basicFileAttributes.creationTime().toMillis();
+            }else if (filesAttribute== FilesAttribute.LAST_ACCESS_TIME){
+                return basicFileAttributes.lastAccessTime().toMillis();
+            }else if (filesAttribute== FilesAttribute.LAST_MODIFIED_TIME){
+                return basicFileAttributes.lastModifiedTime().toMillis();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static Integer getDirFilesSize(Path path) {
+        Map<String, Integer> count = new HashMap<>();
+        count.put("count", 0);
+        try {
+            Files.walkFileTree(path, new CountJavaVisitor(count));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return count.get("count");
+    }
+
+    /**
+     * 并行拷贝目录下所以子文件
+     * @param sourceStr 原目录
+     * @param targetRoot 目标根文件夹
+     * @param deleteSource true：剪切 false:复制
+     * @throws IOException
+     */
+    public static void copyAllParallel(Path sourceStr,String sourceRoot,String targetRoot,boolean deleteSource) {
+        List<Path> allFilesPaths = getAllFilesPaths(sourceStr);
+        allFilesPaths.parallelStream().forEach(x->{
+            try {
+                //备份路径创建
+                Path bakPath = Paths.get(x.getParent().toString().replace(sourceRoot, targetRoot));
+                Files.createDirectories(bakPath);
+                if (deleteSource) {
+                    Files.move(x, bakPath);
+                }else {
+                    //执行复制
+                    Files.copy(x, bakPath,StandardCopyOption.REPLACE_EXISTING);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    /**
+     * 拷贝目录下所以子文件
+     * @param sourceStr 原目录
+     * @param targetStr 目标文件夹
+     * @param deleteSource true：剪切 false:复制
+     * @throws IOException
+     */
+    public static void copyAll(String sourceStr,String targetStr,boolean deleteSource) throws IOException {
+        copyAll(Paths.get(sourceStr), targetStr,deleteSource);
+    }
+    public static void copyAll(Path source,String targetStr,boolean deleteSource) throws IOException {
+        Files.walkFileTree(source, new CopyFindJavaVisitor(source,targetStr,deleteSource));
+    }
+
     public static ResponseEntity<Resource> downloadFile(Path fileRealPath) {
         String filePath = fileRealPath.getFileName().toString();
         Resource resource = null;
@@ -82,9 +175,9 @@ public class FilesUtils {
             contentLength = resource.contentLength();
         } catch (IOException e) {/*ignore*/}
 
-//        if (Log.isInfoEnabled()) {
-//            Log.info("下载文件: {}, 文件大小: {}", fileRealPath, contentLength);
-//        }
+        if (log.isInfoEnabled()) {
+            log.info("下载文件: {}, 文件大小: {}", fileRealPath, contentLength);
+        }
 
         ResponseEntity.BodyBuilder bodyBuilder = ResponseEntity.ok().contentLength(contentLength);
         String dispositionValue;
@@ -104,6 +197,75 @@ public class FilesUtils {
         bodyBuilder.header(HttpHeaders.CONTENT_DISPOSITION, dispositionValue);
         return bodyBuilder.body(resource);
     }
+
+
+    private static class FindJavaVisitor extends SimpleFileVisitor<Path> {
+
+        private List<Path> result;
+        public FindJavaVisitor(List<Path> result) {
+            this.result = result;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+            result.add(file);
+            return FileVisitResult.CONTINUE;
+        }
+
+    }
+    private static class CountJavaVisitor extends SimpleFileVisitor<Path> {
+
+        private Map<String,Integer> count;
+        public CountJavaVisitor(Map count) {
+            this.count = count;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+            count.put("count",count.get("count")+1);
+            return FileVisitResult.CONTINUE;
+        }
+
+    }
+    private static class CopyFindJavaVisitor extends SimpleFileVisitor<Path> {
+        private Path source;
+        private Path target;
+        private boolean deleteSource;
+
+        public CopyFindJavaVisitor(Path source, String target, boolean deleteSource) {
+            this.source = source;
+            this.target = Paths.get(target);
+            this.deleteSource = deleteSource;
+        }
+
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            // 在目标文件夹中创建dir对应的子文件夹
+            Path subDir;
+            if (dir.compareTo(source) == 0) {
+                subDir = target;
+            } else {
+                subDir = target.resolve(dir.subpath(source.getNameCount(), dir.getNameCount()));
+            }
+
+            Files.createDirectories(subDir);
+            return FileVisitResult.CONTINUE;
+        }
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            if (deleteSource) {
+                Files.move(file, target.resolve(file.subpath(source.getNameCount(), file.getNameCount())));
+            }else {
+                Files.copy(file, target.resolve(file.subpath(source.getNameCount(), file.getNameCount())),StandardCopyOption.REPLACE_EXISTING);
+            }
+            return FileVisitResult.CONTINUE;
+        }
+
+    }
     private final static Set<String> imageSuffixs = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
             ".bmp", ".dib", ".gif", ".jfif", ".jpe", ".jpeg", ".jpg", ".png", ".tif", ".tiff", ".ico")));
+
+    public enum FilesAttribute{
+        CREATION_TIME,
+        LAST_MODIFIED_TIME,
+        LAST_ACCESS_TIME;
+    }
 }
--- src/main/java/com/king/hhczy/controller/VSController.java
+++ src/main/java/com/king/hhczy/controller/VSController.java
@@ -1,13 +1,23 @@
 package com.king.hhczy.controller;
 
+import com.king.hhczy.common.util.FilesUtils;
+import com.king.hhczy.common.util.NonStaticResourceHttpRequestHandler;
 import com.king.hhczy.common.util.WordsReading;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 影音类
@@ -20,6 +30,15 @@ public class VSController {
     private String realmName;
     @Value("${server.port}")
     private String port;
+
+    //视频
+    @Autowired
+    private NonStaticResourceHttpRequestHandler nonStaticResourceHttpRequestHandler;
+    //视频扫描路径
+    private static String[] videoPaths = {"H:\\迅雷下载"};
+    //视频格式
+    private static String[] videoFormat = {"MP4","AVI"};
+    private List<Path> paths;
     /**
      * 本地方式上传文件/图片
      *
@@ -32,5 +51,61 @@ public class VSController {
         String com = realmName + ":" + port;
         return wordsReading.build(inWords,com);
     }
+    /**
+     * 预览视频文件, 支持 byte-range 请求
+     */
+    @GetMapping("/v-list")
+    public String videoPath(HttpServletRequest request) throws Exception {
+        String url = realmName +":"+ port;
+        StringBuffer pathStr = new StringBuffer();
+        if (StringUtils.isEmpty(url)) {
+            url = request.getRequestURL().substring(0, request.getRequestURL().indexOf(request.getServletPath()));
+        }
+        url += "/v-play/";
+        //獲取公网IP
+        //访问地址
+        if (paths == null || paths.size() == 0) {
+            List<Path> videoList = new ArrayList<>();
+            for (String videoPath : videoPaths) {
+                //若存在配置的视频格式
+                videoList.addAll(FilesUtils.getAllFilesPaths(videoPath).//找到所有视频路径
+                        stream().filter(//过滤
+                        y -> Arrays.stream(videoFormat).filter(//若路径属于指定视频格式↓
+                                x -> y.toString().toUpperCase().lastIndexOf("." + x.toUpperCase()) != -1).findAny().isPresent()
+                ).collect(Collectors.toList()));//重新收集
+            }
+            paths = videoList.stream().collect(Collectors.toList());
+        }
+        for (int i = 0; i < paths.size(); i++) {
+            String name = paths.get(i).toString();
+            //适配手机显示
+            pathStr.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no\">\n");
+            //拼接播放地址与名称
+            pathStr.append(String.format("<p><a href=\"%s%d\" target=\"_blank\">%d. %s</a></p>", url, i, i + 1, name.substring(name.lastIndexOf("\\") + 1)));
+        }
+        return pathStr.toString();
+    }
 
+    /**
+     * 预览视频文件, 支持 byte-range 请求
+     */
+    @GetMapping("/v-play/{index}")
+    public void videoPreview(@PathVariable int index, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        preview(request, response, paths.get(index));
+    }
+
+    private void preview(HttpServletRequest request, HttpServletResponse response, Path filePath) throws Exception {
+//        String path = request.getParameter("path");
+        if (Files.exists(filePath)) {
+            String mimeType = Files.probeContentType(filePath);
+            if (!StringUtils.isEmpty(mimeType)) {
+                response.setContentType(mimeType);
+            }
+            request.setAttribute(NonStaticResourceHttpRequestHandler.ATTR_FILE, filePath);
+            nonStaticResourceHttpRequestHandler.handleRequest(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+        }
+    }
 }
